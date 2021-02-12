56abc1de4231c7c266fe3fbfefdeea6f086b4926
AuVal <valdiviaaugusto9@gmail.com>
2021-02-11 21:03:51 -0500

    main.tf  s3_module_2/.terraform.lock.hcl

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 # --- main root ---
+
+ # module.stactic_web will be creating resource such aws cloudfront distribution for an S3 bucket with web static files.
 module "stactic_web" {
   source = "./s3_module_1"
   s3_count = 1
   
 }
 
+# module.dns will be creating resource such aws cloudfront origin OAI for an S3 bucket with web static files.
 module "dns" {
   source   = "./s3_module_2"
   s3_count = 1
