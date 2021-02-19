1b002e976480a2ac3d899fb85646ac95ab24710f
AuVal <valdiviaaugusto9@gmail.com>
2021-02-18 20:37:25 -0500

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # --- main root ---
 
- # module.stactic_web will be creating resource such aws cloudfront distribution for an S3 bucket with web static files.
+ # module.static_web_01 will be creating resource such aws cloudfront distribution for an S3 bucket with web static files.
 module "static_web_01" {
   source = "./s3_module_1"
   s3_count = var.bucket_count
   
 }
 
-# module.dns will be creating resource such aws cloudfront origin OAI for an S3 bucket with web static files.
+# module.static_web_02 will be creating resource such aws cloudfront origin OAI for an S3 bucket with web static files.
 module "static_web_02" {
   source   = "./s3_module_2"
   s3_count = var.bucket_count
